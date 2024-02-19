6789473c5efb82ca90a4bc08d49122eee6374fdc
Hkvjr <143500230+Hkvjr@users.noreply.github.com>
2024-02-19 20:43:24 +0200

    Add files via upload

---
+++ paint/js/class/Shape.js
@@ -0,0 +1,14 @@
+export class Shape {
+    _x
+    _y
+
+    constructor(x, y) {
+        this._x = x
+        this._y = y
+        this._lineWidth = this._lineWidth
+    }
+
+set setLineWith(lineWidth) {
+    this._lineWidth = lineWidth
+}
+}
